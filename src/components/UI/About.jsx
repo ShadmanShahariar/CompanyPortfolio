e9b5e3b018a8a31c9b4e8266753b4e86b2ddf2d1
Shadmun Shahariar <109878144+ShadmanShahariar@users.noreply.github.com>
2024-11-04 16:50:30 +0600

    about section start

---
+++ src/components/UI/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../styles/about.css";
+
+const About = () => {
+  return (
+    <section className="about">
+      <div className="container">
+        <div className="about_wrapper">
+          <div className="about_contant">
+            <h6 className="subtitle">Why choose us</h6>
+            <h2>Specialist in advising clients on</h2>
+            <h2 className="highlight"> financial challenges</h2>
+          </div>
+          <div className="about_img"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
