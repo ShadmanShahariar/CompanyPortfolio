a542dfc04fb25e4105e9b172f20585d3b1779d02
Shadmun Shahariar <109878144+ShadmanShahariar@users.noreply.github.com>
2024-11-06 15:11:14 +0600

    'about'

--- src/components/UI/About.jsx
+++ src/components/UI/About.jsx
@@ -10,6 +10,12 @@ const About = () => {
             <h6 className="subtitle">Why choose us</h6>
             <h2>Specialist in advising clients on</h2>
             <h2 className="highlight"> financial challenges</h2>
+            <p className="discription about__content-desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit, soluta, quo cupiditate voluptatibus nostrum earum
+              magnam sed voluptatem iste quaerat quasi labore ut culpa amet
+              delectus porro incidunt dolor recusandae.
+            </p>
           </div>
           <div className="about_img"></div>
         </div>
