39c136c3cc3231740a87a092c560ac02a78441d8
Shadmun Shahariar <109878144+ShadmanShahariar@users.noreply.github.com>
2024-11-06 17:46:54 +0600

    about completed

--- src/components/UI/About.jsx
+++ src/components/UI/About.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import "../../styles/about.css";
 
+import aboutImg from "../../images/about-us.jpg";
+
+const chooseData = [
+  {
+    icon: "ri-wifi-line",
+    title: "Fast working process",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab porro perspiciatis! Numquam, illo? Placeat vitae,necessitatibus unde voluptatibus nihil in.",
+  },
+  {
+    icon: "ri-team-line",
+    title: "Dedicated Team",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab porro perspiciatis! Numquam, illo? Placeat vitae,necessitatibus unde voluptatibus nihil in.",
+  },
+  {
+    icon: "ri-customer-service-2-line",
+    title: "24/7 Support",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab porro perspiciatis! Numquam, illo? Placeat vitae,necessitatibus unde voluptatibus nihil in.",
+  },
+];
+
 const About = () => {
   return (
     <section className="about">
@@ -16,8 +36,23 @@ const About = () => {
               magnam sed voluptatem iste quaerat quasi labore ut culpa amet
               delectus porro incidunt dolor recusandae.
             </p>
+            <div className="choose__item-wrapper">
+              {chooseData.map((item, index) => (
+                <div className="choose__us-item" key={index}>
+                  <span className="choose__us-icon">
+                    <i class={item.icon}></i>
+                  </span>
+                  <div>
+                    <h4 className="choose__us-title">{item.title}</h4>
+                    <p className="discription">{item.desc}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="about_img"></div>
+          <div className="about_img">
+            <img src={aboutImg} alt="" />
+          </div>
         </div>
       </div>
     </section>
