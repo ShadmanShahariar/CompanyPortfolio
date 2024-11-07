ef65fb66fa9b2483868fc0262cbebe8c1af38934
Shadmun Shahariar <109878144+ShadmanShahariar@users.noreply.github.com>
2024-11-07 18:12:48 +0600

    blog start

---
+++ src/components/UI/Blog.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../../styles/blog.css";
+
+const Blog = () => {
+  return (
+    <section className="our__blogs">
+      <div className="container">
+        <div className="blog__top-contant">
+          <h6 className="subtitle"></h6>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Blog;
